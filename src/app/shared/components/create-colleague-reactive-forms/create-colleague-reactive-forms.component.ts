69635663d31b36b9e45c0e71b6b81e2fa43a1e09
Matthieu Charles <matth.charles@protonmail.com>
2023-02-08 16:45:48 +0100

    fix: top-colleagues 06.1 - create-colleague-reactive - seconde partie

--- src/app/shared/components/create-colleague-reactive-forms/create-colleague-reactive-forms.component.ts
+++ src/app/shared/components/create-colleague-reactive-forms/create-colleague-reactive-forms.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators, ValidationErrors, AbstractControl} from "@angular/forms";
 import {createColleagueForm} from "../../../models/createColleagueForm";
 import {ColleagueService} from "../../../providers/colleague.service";
+import {map, Observable} from "rxjs";
 
 
 @Component({
@@ -15,18 +16,40 @@ export class CreateColleagueReactiveFormsComponent {
 
   createColleagueForm = new createColleagueForm();
 
-  constructor(private fb: FormBuilder, private colleagueSrv: ColleagueService) {
+  constructor(private colleagueSrv: ColleagueService, private fb: FormBuilder) {
     this.myReacForm = this.fb.group({
-        prenom: ['', [Validators.required, Validators.minLength(2)]],
-        nom: ['', [Validators.required, Validators.minLength(2)]],
-        pseudo: ['', [Validators.required]],
-        photoUrl: ['', [Validators.required]],
-        submit: ['', []],
-
-    })
+      prenom: ['', [Validators.required, Validators.minLength(2)]],
+      nom: ['', [Validators.required, Validators.minLength(2)]],
+      pseudo: ['',
+        {
+          validators: [Validators.required],
+          asyncValidators: [this.validatePseudo.bind(this)]
+        }],
+      photoUrl: ['', [Validators.required]],
+    }, {validator: this.validateFirstAndLast})
   }
 
+  validateFirstAndLast(control: AbstractControl): ValidationErrors | null {
+    let name = control.value.prenom;
+    let last = control.value.nom;
+    if (name != last) {
+      return null;
+    }
+    return { firstLast : 'first name must be different from lastname' }
+  }
 
+  validatePseudo(control: AbstractControl): Observable<ValidationErrors | null> {
+
+    console.log(control.value);
+
+    return this.colleagueSrv.getColleagueByPseudo(control.value)
+      .pipe(map((isUsed) => {
+          return !isUsed ? null : {
+            pseudoValidator: 'pseudo exists already.'
+          };
+        })
+      );
+  }
   onSubmit() {
     this.createColleagueForm.pseudo = this.myReacForm.get('pseudo')?.value;
     this.createColleagueForm.last = this.myReacForm.get('nom')?.value;
